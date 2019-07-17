1832846f35e3c2d6455712374dc677c70addfe3e
mfiedorowicz <mfiedorowicz@BYDDTPMFIEDOR01>
2019-07-17 14:02:31 +0200

    Regex patterns updated to ver6 (including)

--- AcmeScrubber/CodeExtractor.cs
+++ AcmeScrubber/CodeExtractor.cs
@@ -10,6 +10,9 @@ namespace AcmeScrubber
             {
                 Match match = Regex.Match(metadata, @"(.+_)(.+)(~.+)"); // ver 1&2, group 2
                 //Match match = Regex.Match(metadata, @"(.+_)(.+)(~.+)"); // ver 3, group 5
+                //Match match = Regex.Match(metadata, @"((.+_v[6-9])(.+_)(.+)(~.+))|((.*?~){3}(.*?)(~.+))"); // ver 4, group 3
+                //Match match = Regex.Match(metadata, @"(.+\<Metadata\>)(.*?~){2}(.*?)(~.+)"); // ver 5, group 4
+                //Match match = Regex.Match(metadata, @"//Match match = Regex.Match(metadata, @"(.+\<Metadata\>)(.*?~){2}(.*?)(~.+)"); // ver 5, group 4"); // ver 6, group 4
                 return match.Groups[2].Value;
             }
         }
